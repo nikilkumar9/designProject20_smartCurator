19bebaf5a49c11cf7090038cfcc6e09668931fbe
Nikil Kumar <kumarnikil9865@gmail.com>
2020-04-28 00:02:04 +0800

    firebase added

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,15 +1,3 @@
-// var firebaseConfig = {
-//     apiKey: "AIzaSyClo52cLq4nyObUQl7PdkRpBb4E67z9Q3E",
-//     authDomain: "designproject-f97d8.firebaseapp.com",
-//     databaseURL: "https://designproject-f97d8.firebaseio.com",
-//     projectId: "designproject-f97d8",
-//     storageBucket: "designproject-f97d8.appspot.com",
-//     messagingSenderId: "251561500310",
-//     appId: "1:251561500310:web:27d06ed4cbae22cc0facb7",
-//     measurementId: "G-YZJ0SMV54N"
-// };
-// firebase.initializeApp(firebaseConfig);
-
 // const messaging = firebase.messaging();
 // messaging.requestPermission()
 //     .then(function() {
@@ -19,23 +7,30 @@
 //         console.log('Error Occured!');
 //     })
 
-function goHomePage() {
-    console.log("goHomePAge")
-    document.location.href = './search.html';
-}
+const db = firebase.firestore();
 
-const AWS = require('aws-sdk')
-AWS.config.update({
-    region: 'us-east-1',
-    accessKeyId: '',
-    secretAccessKey: ''
-})
+db.collection("pages")
+    .doc("user_1")
+    .onSnapshot(function(doc) {
+        const data = doc.data();
+        if (data) {
+            const path = data.path;
+            if (document.location.pathname !== path) {
+                document.location.href = path;
+            }
 
-var s3 = new AWS.S3()
-s3.listBuckets(function(err, data) {
-    if (err) {
-        console.log(err, err.stack)
-    } else {
-        console.log(data)
-    }
-})
+            const isSnap = data.snap;
+            if (isSnap) {
+                takeSnapshot();
+                db.doc("pages/user_1").set({
+                    snap: false,
+                }, { merge: true });
+            }
+        }
+        // console.log("Current data: ", doc.data());
+    });
+
+// function goHomePage() {
+//     console.log("goHomePAge");
+//     document.location.href = "./search.html";
+// }
