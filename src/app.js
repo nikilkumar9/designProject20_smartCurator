bf57fce52672501979436859f60031faffd6a006
Nikil Kumar <kumarnikil9865@gmail.com>
2020-04-29 02:36:25 +0800

    added controls from firebase, face-api.js for face-detection

---
+++ src/app.js
@@ -0,0 +1,47 @@
+// const messaging = firebase.messaging();
+// messaging.requestPermission()
+//     .then(function() {
+//         console.log('Have permission');
+//     })
+//     .catch(function(err) {
+//         console.log('Error Occured!');
+//     })
+
+const db = firebase.firestore();
+db.collection("pages")
+    .doc("user_1")
+    .onSnapshot(function(doc) {
+        const data = doc.data();
+        if (data) {
+            const path = data.path;
+            if (document.location.pathname !== path) {
+                document.location.href = path;
+            }
+
+            const isSnap = data.snap;
+            if (isSnap) {
+                takeSnapshot();
+                db.doc("pages/user_1").set({
+                    snap: false,
+                }, { merge: true });
+            }
+
+            if (data.colour) {
+                document.querySelector('#searchinput').value = data.colour;
+            }
+
+            if (data.season) {
+                document.querySelector('#searchinput').value += ' ' + data.season;
+            }
+
+            if (data.type) {
+                document.querySelector('#searchinput').value += ' ' + data.type;
+            }
+        }
+        // console.log("Current data: ", doc.data());
+    });
+
+// function goHomePage() {
+//     console.log("goHomePAge");
+//     document.location.href = "./search.html";
+// }
