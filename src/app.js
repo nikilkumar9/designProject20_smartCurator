96bc836b48a2812ce0778896c3abb2fa9ffed184
Nikil Kumar <kumarnikil9865@gmail.com>
2020-04-30 01:58:00 +0800

    added firebase config for slider.html, modified app.js for Patrick changes

--- src/app.js
+++ src/app.js
@@ -17,7 +17,6 @@ db.collection("pages")
             if (document.location.pathname !== path) {
                 document.location.href = path;
             }
-
             const isSnap = data.snap;
             if (isSnap) {
                 takeSnapshot();
@@ -26,16 +25,28 @@ db.collection("pages")
                 }, { merge: true });
             }
 
+            console.log("Before-", document.querySelector('#criteria').value);
             if (data.colour) {
-                document.querySelector('#searchinput').value = data.colour;
+                document.querySelector('#criteria').value = data.colour;
             }
 
             if (data.season) {
-                document.querySelector('#searchinput').value += ' ' + data.season;
+                document.querySelector('#criteria').value += ' ' + data.season;
             }
 
             if (data.type) {
-                document.querySelector('#searchinput').value += ' ' + data.type;
+                document.querySelector('#criteria').value += ' ' + data.type;
+            }
+            console.log("After-", document.querySelector('#criteria').value);
+
+            const searchButton = data.searchButton;
+            if (searchButton) {
+                document.querySelector('#searchbutton').click();
+                console.log('1')
+                db.doc("pages/user_1").set({
+                    searchButton: false,
+                }, { merge: true });
+                console.log('3');
             }
         }
         // console.log("Current data: ", doc.data());
